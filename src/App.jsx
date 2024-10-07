1aea6e3e7e8cfd0cad54b345945166fd5d0faf1a
Yashraj950 <yashkumar9ipo@gmail.com>
2024-10-07 22:06:47 +0530

    New website

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+import Hero from "./components/Hero/Hero";
+import Products from "./components/Products/Products";
+import AOS from "aos";
+import "aos/dist/aos.css"
+import TopProducts from "./components/TopProducts/TopProducts";
+import Banner from "./components/Banner/Banner";
+import Subscribe from "./components/Subscribe/Subscribe";
+import Testimonials from "./components/Testimonials/Testimonials";
+import Footer from "./components/Footer/Footer";
+
+const App = () => {
+  React.useEffect(() => {
+    AOS.init({
+      offset: 100,
+      duration: 800,
+      easing: "ease-in-sine",
+      delay: 100,
+    });
+    AOS.refresh();
+  }, []);
+  return (
+    <div className="">
+      <Navbar />
+      <Hero />
+      <Products />
+
+      <TopProducts />
+      <Banner />
+      <Subscribe />
+      <Products />
+      <Testimonials/>
+      <Footer/>
+
+    </div>
+  );
+};
+
+export default App;
